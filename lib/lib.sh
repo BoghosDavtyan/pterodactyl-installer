739f998439a2b3fdc628506c27bba6331bf32bc2
BoghosDavtyan <127856427+BoghosDavtyan@users.noreply.github.com>
2024-06-16 20:39:46 +0300

    Update lib.sh

--- lib/lib.sh
+++ lib/lib.sh
@@ -528,6 +528,7 @@ esac
 
 case "$OS" in
 linuxmint)
+  [ "$OS_VER_MAJOR" == "21" ] && SUPPORTED=true
   [ "$OS_VER_MAJOR" == "21.3" ] && SUPPORTED=true
   export DEBIAN_FRONTEND=noninteractive
   ;;
